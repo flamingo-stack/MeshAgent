af196cf4453e8f8ab9957f16bf6a36e50fba256d
Kirill Gontar <Kirill Gontar>
2025-10-04 15:08:07 +0200

    Add virtual terminal

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2025-Oct-4 14:46:23+0200"
-#define SOURCE_COMMIT_HASH "5dbf051f6d70e58157aa504e9c3674a37c30fcaa"
+#define SOURCE_COMMIT_DATE "2025-Oct-4 14:55:51+0200"
+#define SOURCE_COMMIT_HASH "9c2c080f3e55a721b99ff96ec16de10d0fd29152"
