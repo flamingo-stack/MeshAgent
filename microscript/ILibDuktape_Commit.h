c0e69b49eed2e8ad3f841807e8ffdb00b9d8a668
denys-gif <denys@flamingo.cx>
2026-05-25 16:43:28 +0100

    fix: add oss cert verification

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2026-May-21 15:04:19+0100"
-#define SOURCE_COMMIT_HASH "5d19c362e9256568b4ea6d9d7ddedbf775b69bde"
+#define SOURCE_COMMIT_DATE "2026-May-25 15:59:17+0100"
+#define SOURCE_COMMIT_HASH "061c79e3fc5283e10d9165606cbd68fcc3eb1471"
